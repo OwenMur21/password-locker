031bb08f1d8b384bbf4994eddccf539cc4f4057f
Owenmur21 <owenmuriithi@gmail.com>
2018-08-24 11:10:23 +0300

    Runs first test on user to see if a new instance of user is being created

---
+++ test.py
@@ -0,0 +1,27 @@
+import unittest 
+from user import User
+
+class TestUser(unittest.TestCase):
+
+    """
+    Test class that defines test cases for the contact class behaviours
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+
+    """
+    def setUp(self):
+        """
+        Set up method to run before each test case.
+        """
+        self.new_user = User ("Ozzie","21ozwellyeah") #create user object
+        
+
+    def test_init(self):
+        """
+        test_init test case to test if the object is initialized properly
+        """
+        self.assertEqual(self.new_user.username,"Ozzie")
+
+if __name__ == '__main__':
+    unittest.main()
